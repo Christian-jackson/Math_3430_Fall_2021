7e26d2a01b517085b05655239c12020e3ff91a02
Christian-jackson <90790439+Christian-jackson@users.noreply.github.com>
2021-10-19 13:39:19 -0500

    HW#4 download

---
+++ HW#4/HW04(1).py
@@ -0,0 +1,43 @@
+"""
+This assignment is due by 11:59pm on 10/22/2021. 
+
+For this assignment you will be adding functinos to the LA.py script from HW03.
+All functions must satisfy the same requirements as in HW03. The functions you
+will need to add are
+
+#1) A function which takes a scalar as it's input and returns it's absolute
+value. Note that this function must be able to take both real numbers and
+complex numbers as input!!!
+
+#2) A function which takes the as it's arguments
+
+1) A vector stored as a list.
+
+2) A float valued scalar, set to default as 2. 
+
+and returns the p-norm of the input vector. Which p-norm must be determined using
+the float valued scalar input. If no argument is given, it should default to
+2. 
+
+#3) A function which takes as it's argument a vector stored as a list and
+returns the infinity norm of the input vector.
+
+#4) A function which takes as it's arguments
+
+1) A vector stored as a list.
+
+2) An float valued scalar, set to default as 2.
+
+3) A boolean value, set to default as False.
+
+The function will return the p-norm of the input vector. If the boolean value is
+given as True, the function will return the infinity norm of the input vector.
+Otherwise it will return the p-norm of the vector corresponding to the float 
+scalar argument. This function must use the functions from problem #2 and
+problem #3 to earn credit. 
+
+#5) A function which takes as it's arguments two vectors, stored as lists. This
+function then returns the inner product of these vectors. Your function must be
+able to handle complex numbers!
+"""
+
