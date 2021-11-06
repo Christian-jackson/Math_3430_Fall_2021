18a7acabdf8d0ccaa4ad4749820c5730c250d456
Christian-jackson <90790439+Christian-jackson@users.noreply.github.com>
2021-11-05 23:25:02 -0500

    HW#7

--- HW#5/QR.py
+++ HW#5/QR.py
@@ -1,65 +1,5 @@
-"""
-This assignment is due by 11:59pm on 10/29/2021. 
-
-For this assignment you will be writing a python script named QR.py which will
-contain several functions. All functions must satisfy the same requirements as in HW03. 
-
-You will import the LA.py script from HW03 and HW04. You must make use of those
-functions to implement the functions below. Failure to do this will result in an
-earned grade of 0.
-
-For all functions below, matrices will be stored as lists of lists where each
-component list represents a column of the matrix. Use of any other
-representation will result in an earned grade of 0.
-
-The functions you will need to add are
-
-1) A function which implements the unstable version of Gram-Schmidt for reduced QR
-factorization. It will
-take as it's argument a matrix and will return a list of two matrices. The first
-will be Q and the second will be R from the QR factorization described in the
-algorithm.
-
-2) A function which implements the stable version of Gram-Schmidt for reduced QR
-factorization.It will take as it's argument a matrix and will return a list of 
-two matrices. The first will be Q and the second will be R from the QR factorization 
-described in the algorithm.
-
-"""
 
 import LA
-
-def UGS(matrix: list[list]) -> list[list]:
-    """Computes QR factorization using the unstable Gram-Schmidt process.
-        first initializes a square matrix or an R matrix of 0's equal to the matrix input.
-        does the same for V too. then for each element in the input matrix sets equal the element
-        of the input matrix in each element vector of the matrix. the corresponding element of R is set to the inner 
-        product of the vectors of the Q matrix and the V matrix. Then overwrites a variable, A, with the scalar vector multi
-        function of the inner index of q, and the neg outer and inner index of R. Then, overwrite the outer index of V
-        with the add vectors function of the outer index of V and A. The orthoganal matrix Q is then set to be the matrix 
-        V divided by the diagonal scalar elements of R
-
-    Args:
-        matrix_a: A matrix stored as a list of lists.
-    Returns:
-        A list producing the matrices Q and R.
-    """
-
-    Q: list = []
-    V: list = [element for element in matrix]
-    R: list = [([0] * (len(matrix[0]))) for i in range(len(matrix))]
-
-    for outer in range(len(matrix)):
-        V[outer] = matrix[outer]
-        for inner in range(0, outer):
-            R[outer][inner] = LA.inner_product(Q[inner], V[outer])
-            A = LA.scalar_vec_multi(Q[inner], -R[outer][inner])
-            V[outer] = LA.add_vectors(V[outer], A)
-        R[outer][outer] = LA.p_norm((V[outer]))
-        Q.append(LA.scalar_vec_multi((V[outer]), (1/R[outer][outer])))
-    return [Q, R]
-
-
 
 def SGS(matrix: list[list]) -> list[list]:
     """Computes QR factorization using the stable Gram-Schmidt process.
@@ -97,3 +37,19 @@ def SGS(matrix: list[list]) -> list[list]:
             V[inner] = LA.add_vectors(V[inner], A)
     return [Q, R]
 
+def orthonorm(Matrix: list[list]) -> list[list]:
+    '''Orthonormalizes a Matrix
+
+    Put in a matrix stored as a list of lists, then will run the stable
+    Gram-Schmidt function and will return an orthonormalized Q the same span as the
+    input matrix
+
+    Input:
+        Matrix: A matrix stored as a list of lists
+
+    Return:
+        Returns an orthonormalized matrix the same span as the input matrix
+    
+    '''
+    result: list[list] = SGS(Matrix)
+    return result[0]
