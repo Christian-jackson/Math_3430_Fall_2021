ecb71075ad1d13a1eb759f3a2ad163f098070684
Christian-jackson <90790439+Christian-jackson@users.noreply.github.com>
2021-11-28 14:04:54 -0600

    a

---
+++ HW08.py
@@ -0,0 +1,22 @@
+"""
+This assignment is due by 11:59pm on 11/19/2021. 
+
+For this assignment you will be writing a python script named LS.py which will
+contain several functions. All functions must satisfy the same requirements as in HW03. 
+
+You will import the LA.py and QR.py scripts from the previous homeworks. You must make use of those
+functions to implement the functions below. Failure to do this will result in an
+earned grade of 0.
+
+For all functions below, matrices will be stored as lists of lists where each
+component list represents a column of the matrix. Use of any other
+representation will result in an earned grade of 0.
+
+The functions you will need to write are
+
+1) Write a function which takes as it's argument a vector stored as a list, and
+a matrix, and returns the least squares solution, calculated by using QR
+factorization.
+"""
+
+
